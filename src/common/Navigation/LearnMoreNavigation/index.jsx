7ce8c738ca10c90c2c364d449760930be68b537d
sansiven <sansiven@gmail.com>
2022-09-19 15:42:45 +0545

    update logo in learn more page

--- src/common/Navigation/LearnMoreNavigation/index.jsx
+++ src/common/Navigation/LearnMoreNavigation/index.jsx
@@ -6,18 +6,20 @@ import React from "react";
 
 const LearnMoreNavigation = () => (
   <div className="flex justify-between w-auto ml-learn-more-sm md:ml-learn-more-md lg:ml-learn-more">
-    <div className="w-[99px] h-[117px] md:w-[168px] md:h-[198px] bg-303D87 flex flex-col items-center pt-8 md:pt-14">
-      <div className="hidden md:block">
-        <NMALogo />
+    <Link href="/">
+      <div className="w-[99px] h-[117px] md:w-[168px] md:h-[198px] bg-303D87 flex flex-col items-center pt-8 md:pt-14 cursor-pointer">
+        <div className="hidden md:block">
+          <NMALogo />
+        </div>
+        <div className="block md:hidden">
+          <NMALogoSmall />
+        </div>
+        <p className="uppercase text-xxs text-white text-center font-bold tracking-wider mt-2">
+          Neptune Mutual
+          <br /> Association
+        </p>
       </div>
-      <div className="block md:hidden">
-        <NMALogoSmall />
-      </div>
-      <p className="uppercase text-xxs text-white text-center font-bold tracking-wider mt-2">
-        Neptune Mutual
-        <br /> Association
-      </p>
-    </div>
+    </Link>
     <div className="mr-4 md:mr-12 mt-8 md:mt-12 cursor-pointer">
       <Link href="/">
         <Cross />
